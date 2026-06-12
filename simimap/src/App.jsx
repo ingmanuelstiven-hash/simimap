f505e213f7c373dce836a0a16ec711fb19ddb677
ingmanuelstiven-hash <ingmanuelstiven@gmail.com>
2026-06-11 23:56:57 -0500

    Initial commit: Proyecto simimap

---
+++ simimap/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * App.jsx es el componente principal que envuelve toda nuestra aplicación.
+ * Por ahora solo saludaremos para confirmar que la limpieza funcionó.
+ */
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a SimiMap</h1>
+      <p>El proyecto está listo para la Fase 7.</p>
+    </div>
+  )
+}
+
+export default App;
